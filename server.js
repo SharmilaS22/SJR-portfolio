9e8e344f515540d969934b078189b280af02c0e8
SharmilaS22 <52574006+SharmilaS22@users.noreply.github.com>
2020-10-07 15:22:39 +0530

    make it better

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/api/view", require("./routes/api/view"));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client'), 'build', 'index.html');
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 app.listen(process.env.PORT || 5000, (req, res) => {
